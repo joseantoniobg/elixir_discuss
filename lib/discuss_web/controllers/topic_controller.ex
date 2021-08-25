049f81c6cf845c7d0760a9ce376b7f42eb80ae6c
Jose Antonio <joseantonio1@hotmail.com.br>
2021-08-24 17:59:48 -0700

    some small changes

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,9 +2,9 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   def new(conn, params) do
-    IO.puts("++++")
-    IO.puts(conn)
-    IO.puts("++++")
-    IO.puts(params)
+    # IO.puts("++++")
+    # IO.inspect(conn)
+    # IO.puts("++++")
+    # IO.inspect(params)
   end
 end
