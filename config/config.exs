0bb58354787bdd17dca08b81a7c8c38a812512fb
Jose <joseantoniobg@hotmail.com.br>
2021-08-27 16:49:44 -0300

    added auth and user schema

--- config/config.exs
+++ config/config.exs
@@ -29,3 +29,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "d210b94f61ed440ad94e",
+  client_secret: "879a879bf9bc041b1485f53fbc39fdbb957ed820"
